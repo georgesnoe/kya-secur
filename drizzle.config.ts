b59c35bce597213b92cbb6e7c94733d15996ff83
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2026-06-06 11:02:49 +0000

    feat: add initial database configuration and schema definitions

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { config } from 'dotenv';
+config({ path: '.env.local' });
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+    out: './drizzle',
+    schema: './lib/schema.ts',
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: process.env.DATABASE_URL as string,
+    },
+});
